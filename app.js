ac688e3ea30c8fad3c1da0311eb1adf9b7ac2d2e
HBarefoot <henrybarefoot1987@gmail.com>
2017-05-21 20:19:20 -0400

    stopWatch project using JS

---
+++ app.js
@@ -0,0 +1,67 @@
+window.onload = function(){
+  // const outPut = document.getElementById("output")
+  const screen = document.getElementById("screen")
+  const buttonOne = document.getElementById("buttonOne")
+  const buttonTwo = document.getElementById("buttonTwo")
+  const lap = document.getElementById("lap")
+  const time = new Date()
+  let temp
+  let html
+
+  var watch = {
+    minutes: time.getMinutes(),
+    seconds: time.getSeconds(),
+    milliseconds: time.getMilliseconds(),
+    startTime: function(){
+      watch.milliseconds = watch.checkTime(watch.milliseconds)
+      watch.time = screen.innerHTML = addZero(watch.minutes) + ":" +
+      addZero(watch.seconds)  + ":" + addZero(watch.milliseconds)
+      temp = setTimeout(function(){watch.startTime()}, 10)
+    },
+    checkTime: function(ms){
+      if (ms < 99) { ms ++ } else { ms = 0; watch.seconds ++ }
+      if (watch.seconds < 60) { } else { watch.seconds = 0
+        ; watch.minutes = watch.minutes + 1 }
+        return ms
+      }
+    }
+    screen.innerText = "00:00:00"
+    buttonOne.innerText = "Start"
+    buttonTwo.innerText = "Lap"
+    watch.seconds = 0
+    watch.milliseconds = 0
+    watch.minutes = 0
+
+    buttonOne.onclick = function(){
+      if (buttonOne.innerText === "Stop"){
+        buttonOne.innerText = "Start"
+        clearTimeout(temp)
+        buttonTwo.innerText = "Reset"
+      } else {
+        buttonOne.innerText = "Stop"
+        watch.startTime()
+        buttonTwo.innerText = "Lap"
+      }
+    }
+
+    buttonTwo.onclick = function(){
+      if (buttonTwo.innerText === "Lap"){
+        html = '<div class="laps">' + screen.innerText + '</div><hr>'
+         lap.innerHTML += html
+      } else {
+        screen.innerText = "00:00:00"
+        watch.seconds = 0
+        watch.milliseconds = 0
+        watch.minutes = 0
+        lap.innerHTML = ""
+      }
+    }
+
+    function addZero(t){
+      if (t < 10){
+        return t =  "0" + t
+      } else {
+        return t
+      }
+    }
+  }
